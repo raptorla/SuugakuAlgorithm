0e0261198dedbce891d57d548478cbea58a0ba96
raptorla <kotef22raptorla@gmail.com>
2021-12-27 01:31:08 +0900

    add ID009(incomplete)

--- ID009.cpp
+++ ID009.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main(){
-    int N, S;
-    long long sum = 0;
+    long long N, S;
+    vector<long long> a(61); 
     cin >> N >> S;
-    for(int i=1;i <= N;i++){
-        for(int j=1;j <= N;j++){
-            if(i + j <= S){
-                sum ++;
-            }
-        }
+    for(int i=0; i<N; i++) {
+        cin >> a[i];
     }
-    cout << sum << endl;
+    for(long long bit = 0; bit<(1<<N); bit++) {
+        long long sum = 0;
+        for(int j=0; j<N; j++) {
+            if(bit & (1<<j)){
+                sum += a[j];
+            }
 
+        }
+        if(sum == S) {
+            cout << "Yes" << endl;
+            return 0;
+        }
+        
+    }
+    cout << "No" << endl;
+    return 0;
 }
